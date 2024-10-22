fb2a4318896a171e4fbac8260242c60df4d7c29a
Camille <contact.pericat@protonmail.com>
2024-10-22 12:05:42 +0200

    Added home page

--- front/src/pages/Home.jsx
+++ front/src/pages/Home.jsx
@@ -6,9 +6,47 @@ export default function Home() {
   return (
     <div>
       <Navbar />
-      <h1>
-        <Icons.home className="icon" fontSize="large" /> Home
-      </h1>
+      <div className="home">
+        <div classname="left-column">
+          <h1>
+            Bienvenue sur Job Hunter{" "}
+            <a href="https://github.com/ceritrus/ats">
+              <Icons.github fontSize="large" />
+            </a>
+          </h1>
+          <h2>Un job board alimenté par l'intelligence artificielle!</h2>
+          <p>
+            Que vous soyez recruteur ou candidat, profitez d'outils intelligents
+            pour accélerer le processus de recherche.
+            <br />
+            Ce projet a été réalisé par des apprenants de la POE IA de Dawan en
+            Octobre 2024:
+            <ul>
+              <li>
+                <a href="https://www.linkedin.com/in/brannigan-chateau/">
+                  <Icons.linkedin />
+                  Brannigan Chateau
+                </a>
+              </li>
+              <li>
+                <a href="https://www.linkedin.com/in/enzo-onesti/">
+                  <Icons.linkedin />
+                  Enzo Onesti
+                </a>
+              </li>
+              <li>
+                <a href="https://www.linkedin.com/in/camille-pericat/">
+                  <Icons.linkedin />
+                  Camille Péricat
+                </a>
+              </li>
+            </ul>
+          </p>
+        </div>
+        <div className="right-column">
+          <img src="/logo.png" alt="logo" />
+        </div>
+      </div>
     </div>
   );
 }
--- front/src/utils/Icons.jsx
+++ front/src/utils/Icons.jsx
@@ -2,12 +2,16 @@ import PersonIcon from "@mui/icons-material/Person";
 import HomeIcon from "@mui/icons-material/Home";
 import InfoIcon from "@mui/icons-material/Info";
 import WorkIcon from "@mui/icons-material/Work";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import GithubIcon from "@mui/icons-material/Github";
 
 const Icons = {
   user: PersonIcon,
   home: HomeIcon,
   info: InfoIcon,
   work: WorkIcon,
+  linkedin: LinkedInIcon,
+  github: GithubIcon,
 };
 
 export default Icons;
