a9e1974a49881558faecab18ad1f050632bc7d22
Zookan <enzo.onesti@gmail.com>
2024-10-23 10:53:25 +0200

    about candidate structure + read cv

--- front/src/pages/About_candidate.jsx
+++ front/src/pages/About_candidate.jsx
@@ -4,23 +4,23 @@ import '@react-pdf-viewer/core/lib/styles/index.css';
 import '@react-pdf-viewer/default-layout/lib/styles/index.css';
 
 export default function About_candidate() {
-  const pdfUrl = '/path/to/your/pdf-file.pdf'; // Replace with your PDF file path
+  const pdfUrl = 'http://127.0.0.1:8000/api/download/'; // racine du path pour les cv
 
   return (
     <div>
       <Navbar />
       <div className="container">
-        <h1>A propos de </h1>
         <div style={{ display: 'flex'}}>
           <div style={{ width: '40%'}}>
+          <h1>A propos de </h1>
             <p>Date de candidature : </p>
             <p>Score de pertinence : </p>
             <p>Message de motivation : </p>
             <p>Feedback sur la candidature : </p>
           </div>
-          <div style={{ width: '60%', height: '100vh' }}>
+          <div style={{ width: '60%', height: '70vh'}}>
             <Worker workerUrl={`https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.worker.min.js`}>
-                <Viewer fileUrl={pdfUrl} />
+                <Viewer fileUrl={pdfUrl + "1/0/CV_EnzoOnesti.pdf"} />
             </Worker>
           </div>
         </div>
