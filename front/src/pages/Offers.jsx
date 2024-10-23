c154734539bc57a6d1dabed37a7b75404161c123
Camille <contact.pericat@protonmail.com>
2024-10-23 17:12:20 +0200

    Finished offer page

--- front/src/pages/Offers.jsx
+++ front/src/pages/Offers.jsx
@@ -3,26 +3,26 @@ import Navbar from "../components/Navbar";
 import { Fetch } from "../utils/Api";
 import { DataGrid } from "@mui/x-data-grid";
 import Paper from "@mui/material/Paper";
+import { useNavigate } from "react-router-dom";
 
 const columns = [
   { field: "company_description", headerName: "ENTREPRISE", minWidth: 170 },
   { field: "title", headerName: "TITRE", minWidth: 170 },
   { field: "description", headerName: "DESCRIPTION", minWidth: 300 },
+  { field: "job_location", headerName: "LIEU", minWidth: 170 },
   { field: "posted_date", headerName: "PUBLIÉ LE", minWidth: 170 },
   { field: "end_of_application", headerName: "DATE LIMITE", minWidth: 170 },
-  { field: "job_location", headerName: "LIEU", minWidth: 170 },
 ];
 const paginationModel = { page: 0, pageSize: 10 };
 
 export default function Offers() {
   const [data, setData] = useState([]);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const request = async () => {
       try {
-        console.log("Fetching data...");
         const response = await Fetch("/api/v1/job-offer");
-        console.log("Offers: ", response);
         setData(response);
       } catch (error) {
         console.error("Error fetching data:", error);
@@ -44,6 +44,10 @@ export default function Offers() {
             initialState={{ pagination: { paginationModel } }}
             pageSizeOptions={[5, 10, 50]}
             sx={{ border: 0 }}
+            disableColumnSelector={true}
+            onCellClick={(cell) => {
+              navigate(`/offers/${cell.row.id}`);
+            }}
           />
         </Paper>
       </div>
