e6fb404c01f59c264daabbab43860b8e33770c0e
Brannigan CHATEAU <brannigan.pro@gmail.com>
2024-10-28 10:30:08 +0100

    Application added cv_link and new db

--- back/db/models/Application.py
+++ back/db/models/Application.py
@@ -18,6 +18,7 @@ class Application(SQLModel, table=True):
     ats_prenotation: Decimal = Field(default=0, max_digits=3, decimal_places=2)
     ats_final_note : Decimal = Field(default=0, max_digits=3, decimal_places=2)
     feedback: str = Field(sa_column=Column(TEXT))
+    cv_link: str = Field(max_length=100)
     id_candidate: int = Field(foreign_key="candidate.id")    
     id_job_offer: int = Field(foreign_key="joboffer.id")
     candidate: "Candidate" = Relationship(back_populates="applications")
