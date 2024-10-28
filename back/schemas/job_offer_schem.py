0b79479712fa91d78398954b5437643f5a5ee6e6
Brannigan CHATEAU <brannigan.pro@gmail.com>
2024-10-28 10:22:58 +0100

    Added more information in jobOffer

--- back/schemas/job_offer_schem.py
+++ back/schemas/job_offer_schem.py
@@ -1,13 +1,18 @@
 from typing import Optional
 from decimal import Decimal
 import datetime
+from sqlalchemy import Column
+from sqlalchemy import Enum as SQLAlchemyEnum
 from sqlmodel import Field, SQLModel
 from typing import List, Optional
+from back.db.models.JobOffer import Graduate
 
 class JobOfferCreate(SQLModel):
     job_ref: str = Field(..., max_length=12, description="Unique reference for the job offer")
     title: str = Field(..., max_length=50, description="Title of the job offer")
     job_location: str = Field(..., max_length=100, description="Location of the job")
+    graduate: Graduate = Field(sa_column=Column(SQLAlchemyEnum(Graduate)))
+    experience: int = Field(..., default=0)
     salary: Decimal = Field(..., gt=0, description="Salary for the job offer, must be greater than zero")
     description: str = Field(..., description="Detailed description of the job offer")
     company_description: str = Field(..., description="Description of the company")
@@ -21,6 +26,8 @@ class JobOfferUpdate(SQLModel):
     job_ref: Optional[str] = Field(default=None, max_length=12, description="Unique reference for the job offer")
     title: Optional[str] = Field(default=None, max_length=50, description="Title of the job offer")
     job_location: Optional[str] = Field(default=None, max_length=100, description="Location of the job")
+    graduate: Optional[Graduate] = Field(default= None, sa_column=Column(SQLAlchemyEnum(Graduate)))
+    experience: Optional[int] = Field(default=None)
     salary: Optional[Decimal] = Field(default=None, gt=0, description="Salary for the job offer, must be greater than zero")
     description: Optional[str] = Field(default=None, description="Detailed description of the job offer")
     company_description: Optional[str] = Field(default=None, description="Description of the company")
@@ -35,6 +42,8 @@ class JobOfferRead(SQLModel):
     job_ref: str = Field(..., max_length=12, description="Unique reference for the job offer")
     title: str = Field(..., max_length=50, description="Title of the job offer")
     job_location: str = Field(..., max_length=100, description="Location of the job")
+    graduate: Graduate = Field(sa_column=Column(SQLAlchemyEnum(Graduate)))
+    experience: int = Field(..., default=0)
     salary: Decimal = Field(..., gt=0, description="Salary for the job offer, must be greater than zero")
     description: str = Field(..., description="Detailed description of the job offer")
     company_description: str = Field(..., description="Description of the company")
