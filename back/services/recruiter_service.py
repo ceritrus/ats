37d015ad617cd1c774a34aed396c86e0b66cdac6
Brannigan CHATEAU <brannigan.pro@gmail.com>
2024-10-30 09:57:37 +0100

    Added role_id in token

--- back/services/recruiter_service.py
+++ back/services/recruiter_service.py
@@ -4,12 +4,12 @@ from back.services.crud_base import CRUDBase
 from sqlmodel import Session, select
 
 class CRUDRecruiter(CRUDBase[Recruiter, RecruiterCreate, RecruiterRead, RecruiterUpdate]):
-    def check_if_recruiter(self, user_id:int, session: Session) -> bool:
+    def return_role_id(self, user_id:int, session: Session) -> bool:
         statement = select(self.model).where(self.model.id_user == user_id)
         obj = session.exec(statement).first()
         if not obj:
-            return False
+            return None
         else:
-            return True
+            return obj.id
 
 crud_recruiter = CRUDRecruiter(Recruiter, RecruiterRead, RecruiterUpdate)
